1c4b04cf01ea1988cee2e8f9216afc7234b78bb6
Siwarak Kongsra <gtoboo_001@hotmail.com>
2023-11-17 04:10:41 +0700

    Swagger

--- HelloWorld.API/Program.cs
+++ HelloWorld.API/Program.cs
@@ -20,9 +20,9 @@ app.UseSwagger(opt =>
 {
     opt.PreSerializeFilters.Add((swagger, httpReq) =>
     {
-        if (!httpReq.Headers.ContainsKey("X-Forwarded-Prefix")) return;
-        var basePath = "proxy";
-        var serverUrl = $"{httpReq.Scheme}://{httpReq.Headers["X-Forwarded-Prefix"]}/{basePath}";
+        if (!httpReq.Headers.ContainsKey("X-Forwarded-For")) return;
+        string prefix = "api";
+        var serverUrl = $"{httpReq.Scheme}://{httpReq.Host}/{prefix}";
         swagger.Servers = new List<OpenApiServer> { new() { Url = serverUrl } };
     });
 });
