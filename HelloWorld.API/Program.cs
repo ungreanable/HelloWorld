6c96b0cf6f207c92b187c6ef27bcd39d53c1477a
Siwarak Kongsra <gtoboo_001@hotmail.com>
2023-11-17 03:53:42 +0700

    Open Swagger for Public

--- HelloWorld.API/Program.cs
+++ HelloWorld.API/Program.cs
@@ -11,11 +11,11 @@ builder.Services.AddDistributedMemoryCache();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
 
 //app.UseHttpsRedirection();
 
